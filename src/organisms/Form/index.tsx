4f540110dcf1cadef201ca032a2614e33b9926b5
Edilson Aguiar <edilsonjunior3@hotmail.com>
2022-02-06 22:07:58 -0300

    including Yup Validation

--- src/organisms/Form/index.tsx
+++ src/organisms/Form/index.tsx
@@ -3,13 +3,25 @@ import { useForm } from 'react-hook-form'
 import { useState } from 'react';
 import IFormInput from '../../Models/iforminput' 
 import { franchising, routecall } from '../../utils/calculate';
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from "yup";
 
+const schema = yup.object({
+  selectedPlan: yup.string().required(),
+  selectedDddO: yup.number().positive().integer().required(),
+  selectedDddF: yup.number().positive().integer().required(),
+  minutes: yup.number().positive().integer().required(),
+}).required();
 
 const Form = () => {
+    const [noPath, setNoPath ] = useState<string>();
     const [valueWithPlan, setValueWithPlan] = useState<number>();
     const [valueWithoutPlan, setValueWithoutPlan] = useState<number>();
-    const { register, handleSubmit } = useForm<IFormInput>();
+    const { register, handleSubmit, formState:{ errors } } = useForm<IFormInput>({resolver: yupResolver(schema)});
     function onSubmit(data: IFormInput) {
+        if(data.minutes*routecall[data.selectedDddO][data.selectedDddF]*0.9 === 0) {
+            setNoPath('Rota Inexistente!')
+        }else
            setValueWithoutPlan(data.minutes*routecall[data.selectedDddO][data.selectedDddF]*0.9);
         if(data.minutes>franchising[data.selectedPlan]){
            setValueWithPlan((data.minutes-franchising[data.selectedPlan])*routecall[data.selectedDddO][data.selectedDddF]);
@@ -28,27 +40,26 @@ const Form = () => {
               <option value="FaleMais60">FaleMais60</option>
               <option value="FaleMais120">FaleMais120</option>
           </S.FormSelect>
-        <br/>  
-      <label>DDD Origin
+      <br/>  
+      <label>DDD Origin</label>
       <S.FormSelect {...register("selectedDddO")}>
               <option value="11">11</option>
               <option value="16">16</option>
               <option value="17">17</option>
               <option value="18">18</option>
-          </S.FormSelect>
-    </label>
+      </S.FormSelect>
     <br/>      
-      <label>DDD Destiny
+      <label>DDD Destiny</label>
         <S.FormSelect {...register("selectedDddF")}>
               <option value="11">11</option>
               <option value="16">16</option>
               <option value="17">17</option>
               <option value="18">18</option>
         </S.FormSelect>
-      </label>
-      <br/>    
+    <br/>    
       <label>Minutos</label>
       <S.FormInput {...register("minutes")}/>
+      {errors.minutes && <p>{errors.minutes.message}</p>}
       <br/>  
       <S.FormButton type="submit">Calculate</S.FormButton>
       <S.PriceColumn>
